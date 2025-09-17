6ef2c999d086ca830e07c5d00d6c5a7eddeddcb5
SyedaBismaa <syedabisma173@gmail.com>
2025-09-17 15:08:07 +0530

    Ui setup

---
+++ frontend/src/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import React from 'react'
+
+
+const AppRoutes = () => {
+  return (
+    <div>AppRoutes</div>
+  )
+}
+
+export default AppRoutes
